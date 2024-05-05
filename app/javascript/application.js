1d1e9867a417d40fc2b62e44583c0d170607c6d2
katsuhiro sugada <sugada.katsuhiro@gmail.com>
2024-05-05 14:35:48 +0900

    css-refactoring-1

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,6 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-
 import jquery from "jquery"
 window.$ = jquery
 
-
 $(function () {
-  $('.notice').fadeOut(4000);  //４秒かけて消えていく
+  $('.notice').fadeOut(10000);  //４秒かけて消えていく
 });
