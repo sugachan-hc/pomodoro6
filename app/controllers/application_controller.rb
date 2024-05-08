e3a4c950a2352a291add3b90147421e07fd5fa50
katsuhiro sugada <sugada.katsuhiro@gmail.com>
2024-05-08 21:55:13 +0900

    modify how private is written, set 'Show full error reports' back to default.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,17 @@ class ApplicationController < ActionController::Base
     rescue_from ActionController::ParameterMissing, with: :rescue_bad_request
   end
 
-  private def rescue_bad_request(exception)
+  private 
+  
+  def rescue_bad_request(exception)
     render "errors/bad_request", status: 400, formats: [:html]
   end
 
-  private def rescue_not_found(exception)
+  def rescue_not_found(exception)
     render "errors/not_found", status: 404, formats: [:html]
   end
 
-  private def rescue_internal_server_error(exception)
+  def rescue_internal_server_error(exception)
     render "errors/internal_server_error", status: 500, formats: [:html]
   end
 end
