32135493db4061b4ccf10290c3be6baac4ebaef8
katsuhiro sugada <sugada.katsuhiro@gmail.com>
2024-05-11 16:27:46 +0900

    add db production settings

---
+++ bin/render-build.sh
@@ -0,0 +1,16 @@
+t on error
+set -o errexit
+
+echo "render-bulid.sh: start"
+
+echo "installing bundle"
+bundle install
+
+echo "precompiling assets"
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+
+echo "executing migrate"
+bundle exec rails db:migrate
+
+echo "render-build.sh: done"
